39272cde366c6e7741a65e53cd55a6d3441f2083
Nguyễn Đặng Hùng <73513692+oldjack247@users.noreply.github.com>
2021-10-28 11:15:09 +0900

    first year game

---
+++ Box_Heroes/ソースファイル/Box_Heroes/light.h
@@ -0,0 +1,13 @@
+#ifndef _LIGHT_H_
+#define _LIGHT_H_
+#include "main.h"
+
+//==============================================================================
+// プロトタイプ宣言
+//==============================================================================
+void InitLight(void);		// ライトの初期化処理
+void UninitLight(void);		// ライトの終了処理
+void UpdateLight(void);		// ライトの更新処理
+D3DLIGHT9 *GetLight(void);
+
+#endif
